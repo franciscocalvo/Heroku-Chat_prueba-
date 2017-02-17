ac4e0136a426c213e40b68001af3dcbe01d32582
Francisco Javier Calvo <franciscojaviercalvoserrano@gmail.com>
2017-02-17 09:48:39 +0100

    Correción de fallo

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('public/html/index.html');
-});
 
-app.use(app.static("public"));
+app.use(express.static("public"));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
